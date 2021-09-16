44a1f9be1a1aeb7b498a9acb500a4b498ab67945
amandabiagi <amanda.biagi@bandtec.com.br>
2021-09-16 10:55:21 -0300

    Criação do projeto

---
+++ DesafioMagaLab/DesafioMagaLab/src/main/java/the/biagi/DesafioMagaLab/DesafioMagaLabApplication.java
@@ -0,0 +1,13 @@
+package the.biagi.DesafioMagaLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMagaLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMagaLabApplication.class, args);
+	}
+
+}
---
+++ DesafioMagaLab/DesafioMagaLab/src/test/java/the/biagi/DesafioMagaLab/DesafioMagaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package the.biagi.DesafioMagaLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioMagaLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
