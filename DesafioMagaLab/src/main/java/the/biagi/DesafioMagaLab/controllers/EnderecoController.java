fb3fd696e6be04a53d51737dcf1ef985e674e2aa
amandabiagi <amanda.biagi@bandtec.com.br>
2021-09-17 12:37:49 -0300

    Criando e registrando logs no arquivo logs.txt

--- DesafioMagaLab/src/main/java/the/biagi/DesafioMagaLab/controllers/EnderecoController.java
+++ DesafioMagaLab/src/main/java/the/biagi/DesafioMagaLab/controllers/EnderecoController.java
@@ -1,18 +1,21 @@
 package the.biagi.DesafioMagaLab.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import the.biagi.DesafioMagaLab.model.Endereco;
 import the.biagi.DesafioMagaLab.service.EnderecoService;
 import the.biagi.DesafioMagaLab.viacep.ViaCepService;
 
 @RestController
 @RequestMapping("/endereco")
 public class EnderecoController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EnderecoController.class);
 
     @Autowired
     ViaCepService viaCepService;
@@ -24,8 +27,10 @@ public class EnderecoController {
     public ResponseEntity<?> cadastrarEndereco(@PathVariable String cep) {
 
         if (!enderecoService.validarCep(cep)) {
+            LOGGER.info("Cep invalido.");
             return ResponseEntity.status(404).body("CEP inválido");
         }
+        LOGGER.info("Retorno de endereço com sucesso");
         return enderecoService.enderecoExistente(cep);
 
     }
